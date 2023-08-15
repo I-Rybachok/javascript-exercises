7ce768d49342d97bc1ea345b5fb73da68ea60a26
Ірина Рибачок <113577981+I-Rybachok@users.noreply.github.com>
2023-08-15 17:28:22 +0300

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,29 +1,20 @@
-// const repeatString = function(string, num) {
-//   let result = '';
+const repeatString = function(string, num) {
+  let result = '';
 
-//   if (num < 0){
-//     return 'ERROR';
-//   }
-
-//   if (string === ''){
-//     break;
-//   }
-//   for (let i = 0; i < num; i++){
-//     result = result + string;
-//   }
-  
-//   return result;
-// }
-// repeatString('hey', 3);
-
-const repeatString = function (word, times) {
-  if (times < 0) return "ERROR";
-  let string = "";
-  for (let i = 0; i < times; i++) {
-    string += word;
+  if (num < 0){
+    return 'ERROR';
   }
-  return string;
-};
+
+  if (string === ''){
+    break;
+  }
+  for (let i = 0; i < num; i++){
+    result = result + string;
+  }
+  
+  return result;
+}
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
