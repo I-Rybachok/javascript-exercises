2517c37e4c7b1e9664793fe3532467501a02c689
Ірина Рибачок <113577981+I-Rybachok@users.noreply.github.com>
2023-08-16 13:22:46 +0300

    Update reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let arr = [];
+  
+  for (let i = 0; i < string.length; i++) {
+    arr = string[i] + arr;
+  }
 
+  return arr;
 };
 
 // Do not edit below this line
