3806245ee87cc555d97c121557c4b49b7a6517a7
Ірина Рибачок <113577981+I-Rybachok@users.noreply.github.com>
2023-08-16 13:54:19 +0300

    Update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, num) {
+  let arr_result = [];
+  
+  for (let i = 0; i < array.length; i++) {
+    if (arr[i] === num) {
+      continue;
+    } else {
+      arr_result.push(i)
+    }
+  }
+return arr_result;
 };
 
 // Do not edit below this line
